d006e6199f8d8fe4fc9744016e9ae670fe9ec53d
Alassane Fofana <74676173+Las07@users.noreply.github.com>
2022-05-07 07:23:34 +0200

    Update [slug].tsx

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -164,7 +164,7 @@ const Post = ({ post }: Props) => {
           />
         </form>
       )}
-      <div className="my-10 mx-auto flex max-w-2xl flex-col space-y-2 p-10 shadow shadow-yellow-500">
+      <div className="max-w-2xl$^ my-10 mx-auto flex flex-col space-y-2 p-10 shadow shadow-yellow-500">
         <h3 className="text-4xl">Comments</h3>
         <hr className="pb-2" />
         {post.comments.map((comment) => (
